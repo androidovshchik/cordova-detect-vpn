42bf460a14c7612155277c2fb5aa5b50815bc09b
vlad <vladkalyuzhnyu@gmail.com>
2020-07-22 16:59:55 +0300

    changes

---
+++ hooks/manifest.js
@@ -0,0 +1,13 @@
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path');
+    var platformPath = path.join(ctx.opts.projectRoot, 'platforms/android');
+    var manifestPath = path.join(platformPath, 'AndroidManifest.xml');
+    if (!fs.existsSync(manifestPath)) {
+        manifestPath = path.join(platformPath, 'app/src/main/AndroidManifest.xml');
+    }
+    var oldManifest = fs.readFileSync(manifestPath, 'utf8');
+    var newManifest = oldManifest.replace(/<action\s+android:name="android.intent.action.MAIN"\s*\/>/g, '')
+        .replace(/<category\s+android:name="android.intent.category.LAUNCHER"\s*\/>/g, '');
+    fs.writeFileSync(manifestPath, newManifest, 'utf-8');
+};
